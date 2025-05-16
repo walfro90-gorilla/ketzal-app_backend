11cd832c27ba7d4e78927f0561c34e7123f46a73
Wal <walfre.am@gmail.com>
2025-05-16 11:52:09 -0600

    back end sintaxis fixed

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import serverlessExpress from 'serverless-http';
+import { Handler, Context, APIGatewayEvent } from 'aws-lambda';
+
+const expressApp = express();
+let server: any;
 
 async function bootstrap() {
-
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
     app.setGlobalPrefix('api');
 
     const config = new DocumentBuilder()
@@ -17,8 +23,15 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, documentFactory);
 
     app.enableCors();
-
-    await app.listen(4000);
+    await app.init();
+    server = serverlessExpress(expressApp);
 }
 
-bootstrap()
+bootstrap();
+
+export const handler = async (event: any, context: any) => {
+    if (!server) {
+        await bootstrap();
+    }
+    return server(event, context);
+};
