4ba76ea1209d8c9425d562ac9ad0574f7609177d
Wal <walfre.am@gmail.com>
2025-05-19 09:37:38 -0600

    health link

--- src/main.ts
+++ src/main.ts
@@ -19,11 +19,13 @@ async function bootstrap() {
 
     app.enableCors();
     // Add health check endpoint using Express instance with proper types
-    app.getHttpAdapter().getInstance().get('/health', (req: Request, res: Response) => res.send('ok')
-    );
+    app.getHttpAdapter().getInstance().get('/health', (req: Request, res: Response) => res.send('ok'));
+    // Endpoint de healthcheck en la raíz
+    app.getHttpAdapter().getInstance().get('/', (req: Request, res: Response) => res.send('ok'));
 
-
+    console.log('Antes de app.listen');
     await app.listen(process.env.PORT || 4000, '0.0.0.0');
+    console.log('Después de app.listen');
 }
 
 bootstrap()
